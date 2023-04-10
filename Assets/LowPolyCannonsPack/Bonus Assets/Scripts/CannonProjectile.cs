7eebbd10f209af94ec4cf8d141c2bdcf020a9010
Athean <eroyant@gmail.com>
2023-04-10 23:18:49 +0200

    add cannons assets

---
+++ Assets/LowPolyCannonsPack/Bonus Assets/Scripts/CannonProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonProjectile : MonoBehaviour
+{
+
+    public float fireSpeed=400;
+    public ParticleSystem fireParticle;
+    void Start()
+    {
+        StartCoroutine(FireProjectileCoroutine());
+    }
+
+    IEnumerator FireProjectileCoroutine()
+    {
+        yield return new WaitForSeconds(4.0f);
+
+        FireProjectile();
+
+        yield return null;
+    }
+
+    public void FireProjectile()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+        fireParticle.Play();
+        GetComponent<Rigidbody>().AddForce(transform.forward * fireSpeed);
+    }
+
+}
