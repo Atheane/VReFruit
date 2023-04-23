c54af01e00baafc010967f5aa0abbdf8bef4c82e
Athean <eroyant@gmail.com>
2023-04-23 17:19:01 +0200

    load fruits prefabs

--- Assets/Scripts/InteractionFXSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "InteractionFXSO", menuName = "ScriptableObjects/InteractionFXSO", order = 3)]
-public class InteractionFXSO : ScriptableObject
-{
-    public GameObject handFx;
-    public GameObject rayFx;
-}
-
